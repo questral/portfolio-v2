be6d339ca02e273ecfa1f3b1ec3f6a45bc24d0a8
questral <158232605+questral@users.noreply.github.com>
2025-01-12 14:55:28 -0500

    updated config settings and base path stuff so routes work in Github pages (#1)
    Co-authored-by: aokorodu <aokorodu@gmail.com>

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,38 +1,36 @@
 <script>
-    let { children } = $props();
+  let { children } = $props();
+  import { base } from "$app/paths";
 </script>
 
-
 <div class="container">
-    <nav>
-        <a class="nametag" href="/">kai okorodudu</a>
-        <a href="/contact">contact</a>
-        <a href="/motion">motion</a>
-        <a href="/code">code</a>
-        <a href="/about">about</a>
-    </nav>
-    {@render children()}
+  <nav>
+    <a class="nametag" href="{base}/">kai okorodudu</a>
+    <a href="{base}/contact">contact</a>
+    <a href="{base}/motion">motion</a>
+    <a href="{base}/code">code</a>
+    <a href="{base}/about">about</a>
+  </nav>
+  {@render children()}
 </div>
 
 <style>
-    :global {
-        a {
-            color: black;
-            text-decoration: underline;
-            font-weight: 800;
-        }
+  :global {
+    a {
+      color: black;
+      text-decoration: underline;
+      font-weight: 800;
     }
-    
+  }
 
-    .container {
-        font-family: "IBM Plex Mono", serif;
-        max-width: 1000px;
-        margin: 1rem auto;
-        padding: 1rem;
+  .container {
+    font-family: "IBM Plex Mono", serif;
+    max-width: 1000px;
+    margin: 1rem auto;
+    padding: 1rem;
+  }
 
-    }
-
-    .nametag {
-        font-size: 2rem;
-    }
+  .nametag {
+    font-size: 2rem;
+  }
 </style>
